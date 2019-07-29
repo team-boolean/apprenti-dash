d124151f17e053b59e67b99347cfa5e4ce513062
Peter Tynan <pettynan@amazon.com>
2019-07-29 12:07:15 -0700

    added thread findById

--- src/main/java/com/example/teamboolean/apprentidash/Repos/ThreadRepository.java
+++ src/main/java/com/example/teamboolean/apprentidash/Repos/ThreadRepository.java
@@ -4,5 +4,5 @@ import com.example.teamboolean.apprentidash.Models.Comment;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ThreadRepository extends CrudRepository<Thread, Long> {
-    
+    Thread findById(long id);
 }
