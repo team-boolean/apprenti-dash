eb184cad7a4532ec071e1c361ec3c89bca6cb602
Peter Tynan <pettynan@amazon.com>
2019-07-29 15:07:28 -0700

    functioning post requests

--- src/main/java/com/example/teamboolean/apprentidash/Controllers/ForumController.java
+++ src/main/java/com/example/teamboolean/apprentidash/Controllers/ForumController.java
@@ -1,12 +1,14 @@
 package com.example.teamboolean.apprentidash.Controllers;
 
 import com.example.teamboolean.apprentidash.Models.AppUser;
+import com.example.teamboolean.apprentidash.Models.Comment;
 import com.example.teamboolean.apprentidash.Models.Discussion;
 import com.example.teamboolean.apprentidash.Repos.AppUserRepository;
 import com.example.teamboolean.apprentidash.Repos.CommentRepository;
 import com.example.teamboolean.apprentidash.Repos.DiscussionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -34,5 +36,15 @@ public class ForumController {
         return new RedirectView("/forum/" + newDiscussion.getId());
     }
 
+    @PostMapping("/forum/{id}")
+    public RedirectView createComment(@PathVariable long id, Principal p, String body) {
+        AppUser author = appUserRepository.findByUsername(p.getName());
+        Discussion parentDiscussion = discussionRepository.findById(id);
+        Comment newComment = new Comment(author, parentDiscussion, body);
+
+        commentRepository.save(newComment);
+
+        return new RedirectView("/forum/" + id);
+    }
 
 }
