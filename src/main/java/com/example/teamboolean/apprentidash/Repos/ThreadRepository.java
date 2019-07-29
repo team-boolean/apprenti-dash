fed42dadd9580bcacc913f21f347e07cf241cce4
Peter Tynan <pettynan@amazon.com>
2019-07-29 11:37:28 -0700

    created default repos

---
+++ src/main/java/com/example/teamboolean/apprentidash/Repos/ThreadRepository.java
@@ -0,0 +1,8 @@
+package com.example.teamboolean.apprentidash.Repos;
+
+import com.example.teamboolean.apprentidash.Models.Comment;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ThreadRepository extends CrudRepository<Thread, Long> {
+    
+}
