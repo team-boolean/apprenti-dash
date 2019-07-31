e8e2d48e798e454141c60f7628a3f6d16eb1a9f5
Peter Tynan <pettynan@amazon.com>
2019-07-31 15:33:47 -0700

    added controller to delete discussion post

--- src/main/java/com/example/teamboolean/apprentidash/Repos/CommentRepository.java
+++ src/main/java/com/example/teamboolean/apprentidash/Repos/CommentRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+    List<Comment> findAllByParentDiscussion(Discussion parentDiscussion);
 }
